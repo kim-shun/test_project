78060137d433ea6eacc773206f9399171068d0d9
kim-shun <kimura1417080@gmail.com>
2022-04-19 22:15:59 +0900

    データを表示

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    """テストモデル"""
+    name = models.CharField(verbose_name='名前', max_length=40)
+    age = models.PositiveIntegerField(verbose_name='年齢')
+    title = models.CharField(verbose_name='タイトル', max_length=40)
+    content = models.TextField(verbose_name='本文', blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Test'
+
+    def __str__(self):
+        return self.title
--- test_app/views.py
+++ test_app/views.py
@@ -1,4 +1,10 @@
 from django.views import generic
+from .models import Test
 
-class IndexView(generic.TemplateView):
+class IndexView(generic.ListView):
+  model = Test
   template_name = "index.html"
+
+  def get_queryset(self):
+    tests = Test.objects.all().order_by('-created_at')
+    return tests
